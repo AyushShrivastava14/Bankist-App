da51138aa052112cee67bb0ed7f9b1bab84cd3f1
Ayush Shrivastava <ayush.shrivastava.023@gmail.com>
2024-07-06 07:34:59 +0100

    Transaction Functioning

--- script.js
+++ script.js
@@ -76,3 +76,35 @@ const displayMovements = function(movements) {
 }
 
 displayMovements(account1.movements);
+
+
+const calcDisplayBalance = function(movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} Rs`;
+}
+
+calcDisplayBalance(account1.movements);
+
+
+const calcDisplaySummary = movements => {
+  const income = movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${income} Rs`;
+
+  const out = movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)} Rs`;
+
+  const interest = movements.filter(mov => mov > 0)
+  .map(deposit => (deposit * 1.2) / 100).filter(int => int >= 1)
+  .reduce((acc, int) => acc + int, 0);
+
+  labelSumInterest.textContent = `${interest} Rs`;
+}
+
+calcDisplaySummary(account1.movements);
+
+
+const createUsernames = function(accs) {
+  accs.forEach(acc =>  acc.username = acc.owner.toLowerCase().split(' ').map(Name => Name[0]).join(''));
+}
+
+createUsernames(accounts);
